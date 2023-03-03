c040abad004467e48163060bb5ec12460fbdcaa3
Gabriel Maciel <gabrielc.maciel42@gmail.com>
2023-03-03 16:22:39 -0300

    initial commit

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.db = router.db;
 
 const rules = auth.rewriter({
   users: 600,
+  houses: 644,
+  rents: 644
 });
 
 app.use(cors());
@@ -19,5 +21,8 @@ app.use(router);
 app.listen(port);
 
 console.log("Server is running on port:", port);
+//Auxililar no desenvolvimento, remover na produção:
+console.log(`baseURL:`);
+console.log(`http://localhost:${port}`);
 
 /* A senha do Kenzinho é 123456 */
